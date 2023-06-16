642ecd3002344da87980c8af1f36e965fa9e0cdb
arif wijanarko <arifpethod@gmail.com>
2023-06-16 10:14:47 +0700

    tugas javasrcipt
    untuk niomic

---
+++ tugas27.js
@@ -0,0 +1,18 @@
+var a = "No";
+var b = "rumah saya";
+var c = 30;
+var d = "A";
+var e = "Jalan pangeran Tirtayasa No";
+var f = 70;
+
+var konversA = String(a);
+var konversb = String(b);
+var konversc = String(c);
+var konversd = String(d);
+var konverse = String(e);
+var konversf = String(f);
+
+var gabung = konversA + " " + konversb + " " + konversc + konversd;  
+var gabung2 = konverse + " " + konversf;
+console.log(gabung);
+console.log(gabung2);
